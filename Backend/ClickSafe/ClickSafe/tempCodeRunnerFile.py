c516543ce88cc4d9238b2668384d0dbd620ea949
ABHAY-st2580 <abhaysinghtomar835@gmail.com>
2026-03-24 23:32:58 +0530

    views file updated

---
+++ Backend/ClickSafe/ClickSafe/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+
+# try:
+#   model_predicition = model.predict(url_array)
+#   print(model_predicition[0])
+#   if model_predicition[0] == 1:
+#     result = 'Phishing'
+#   else:
+#     result = 'Legitimate'
+# except Exception as e:
+#   print('Model Fails')
