fb778ad3ac49d19cf4ad219928ad66d0892eb864
ABHAY-st2580 <abhaysinghtomar835@gmail.com>
2026-03-25 21:42:10 +0530

    MySQL database is connected

--- Backend/ClickSafe/ClickSafe/settings.py
+++ Backend/ClickSafe/ClickSafe/settings.py
@@ -79,10 +79,22 @@ WSGI_APPLICATION = "ClickSafe.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'URLS',
+        'USER': 'root',
+        'PASSWORD': 'abhayst2580',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
